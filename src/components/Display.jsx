a40a731bcd27cbe06840435f257f3bdc269171d5
Daniel Santos Sobrinho <127433077+D-nss@users.noreply.github.com>
2025-01-10 13:10:39 -0300

    Update Display.jsx

--- src/components/Display.jsx
+++ src/components/Display.jsx
@@ -1,5 +1,24 @@
-import React from 'react'
-import './Display.css'
+import React, { useRef } from 'react';
+import './Display.css';
 
-export default props =>
-    <div className='display'>{props.value}</div>
+export default function Display(props) {
+  const displayRef = useRef(null);
+
+  const handleWheel = (event) => {
+    event.preventDefault();
+
+    const scrollAmount = event.deltaY;
+    displayRef.current.scrollLeft += scrollAmount;
+  };
+
+  return (
+    <div
+      ref={displayRef}
+      className="display"
+      onWheel={handleWheel} 
+      scroll-smooth
+    >
+      {props.value}
+    </div>
+  );
+}
